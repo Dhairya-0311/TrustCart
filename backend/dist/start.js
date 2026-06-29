49de0df84295593be3071d2ba80be841bdf23a2a
Dhairya-0311 <dhairyaborse@gmail.com>
2026-06-29 20:51:31 +0530

    add error wrapper

---
+++ backend/dist/start.js
@@ -0,0 +1,9 @@
+﻿process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err);
+  process.exit(1);
+});
+process.on('unhandledRejection', (err) => {
+  console.error('UNHANDLED REJECTION:', err);
+  process.exit(1);
+});
+require('./index.js');
